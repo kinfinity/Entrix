6d1611c79583d52e66c7837fa9f8262054271fee
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-02-21 17:41:58 -0700

    upgrade build

--- ci/scripts/code/build-poetry-project.sh
+++ ci/scripts/code/build-poetry-project.sh
@@ -16,20 +16,20 @@ pushd ${PROJECT_DIR} > /dev/null
 ROOT_MODULE_DIR=$(ls src)
 PACKAGE_NAME=${ROOT_MODULE_DIR}
 
-mkdir -p reports
+mkdir -p reports # for test reports
 
 poetry install
 poetry run pylint ${ROOT_MODULE_DIR}
+
 # --check only in GHA
 # poetry run black --check .  
 # poetry run isort --check . 
+
 # local build
 poetry run black . 
 poetry run isort . 
-poetry run mypy --junit-xml=../reports/${PACKAGE_NAME}-junit.xml .
 
-# no tests exists for lambdas
-# [ -d ${ROOT_MODULE_DIR}/tests ] & poetry run pytest --junitxml=../reports/${PACKAGE_NAME}-junit.xml
-
+[ -d ${ROOT_MODULE_DIR}/tests ] & poetry run pytest --junitxml=../reports/${PACKAGE_NAME}-junit.xml
+poetry run mypy --ignore-missing-imports --junit-xml=../reports/${PACKAGE_NAME}-junit.xml . 
 
 popd > /dev/null
