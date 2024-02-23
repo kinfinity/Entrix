4b78e12e241115404701234e8c7b4520fd2bca18
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-02-22 17:09:48 -0700

    setup cdk in pipeline

--- bin/entrix.ts
+++ bin/entrix.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib'
+import { App } from 'aws-cdk-lib'
 import { EntrixStack } from '../lib/entrix-stack'
 
 // Initialize Application and Stack for Entrix Orders
-const app = new cdk.App()
-new EntrixStack(app, 'Orders')
+const app = new App()
+new EntrixStack(
+    app,
+    'Orders',
+    {
+        env: {
+            account: process.env.AWS_ACCOUNT_ID,
+            region: process.env.AWS_DEFAULT_REGION,
+        }
+    }
+)
